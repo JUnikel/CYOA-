cd5fc2e0841e809c27fb69dc7aa5ac836b4e49a4
Annika Johnson <annikajohnson99@gmail.com>
2016-04-27 15:01:24 -0500

    controll.rb additions

--- controller.rb
+++ controller.rb
@@ -6,6 +6,10 @@ end
 
 get '/startingpage' do
 	@description = "You are about to go to school. You hear on the news that it is very windy and there is a high chance of storms. What do you do?"
+	option_a = "Don't_go"
+	option_b = "Drive"
+	option_c = "Train"
+	option_d = "Bus"
 	erb :startingpage
 end
 
@@ -13,6 +17,27 @@ get '/dontgo' do
 	erb :dontgo
 end
 
-get 'drive' do
+get '/drive' do
+	@description = "Oh no! Your car breaks down, you have to walk now."
 	erb :drive
 end
+
+get '/bus' do 
+	@description = "You arrive at the station before catching your ride and the CTA has shut down."
+	@option_a = "Go home"
+	@option_b = "Wait it out"
+	erb :bus 
+end 
+
+get '/train' do 
+	@description = "You arrive at the station before catching your ride and the CTA has shut down." 
+	@option_a = "Go home"
+	@option_b = "Wait it out"
+	erb :train
+end 
+
+
+
+<p> <%= @bus %> <p>
+
+<a href=<%= "/di"
